c7c58ae03474948f978c907500c09341f641f5fa
like_wen <2279719702@qq.com>
2022-10-15 18:54:30 +0800

    添加部分页面逻辑

--- Client/src/main/java/com/lkw/client/ClientController/LoginController.java
+++ Client/src/main/java/com/lkw/client/ClientController/LoginController.java
@@ -10,6 +10,8 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -22,8 +24,13 @@ public class LoginController {
     private TextField password;
     @FXML
     private TextField username;
+    @FXML
+    private Text tips;
 
     public LoginController(){
+        //创建ip
+        //获取连接
+
 
     }
 
@@ -31,14 +38,29 @@ public class LoginController {
 
     public void loginBtn(){
         //登录事务处理
+        String passwordText = password.getText();
+        String usernameText = username.getText();
+        //查询数据库
 
-        //
-        Platform.runLater(()->{
-            System.out.println("登录成功,跳转ing");
-            Stage primaryStage = (Stage)username.getScene().getWindow();
-            primaryStage.hide();
-            new MainStart().start(primaryStage);
-        });
+        if(false) {
+            //存在
+
+
+            //跳转
+            Platform.runLater(() -> {
+                System.out.println("登录成功,跳转ing");
+                Stage primaryStage = (Stage) username.getScene().getWindow();
+                primaryStage.hide();
+                new MainStart().start(primaryStage);
+            });
+
+        }else{
+            //不存在
+            //输出提示信息
+            tips.setText("你输入的用户名或密码不正确,请重新输入");
+
+        }
+
 
     }
 
--- Client/src/main/java/com/lkw/client/ClientController/MainController.java
+++ Client/src/main/java/com/lkw/client/ClientController/MainController.java
@@ -2,14 +2,21 @@ package com.lkw.client.ClientController;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
+import javafx.scene.web.WebView;
 
 public class MainController {
     @FXML
     private TextArea sendArea;
 
+    @FXML
+    private TextArea acceptArea;
+
 
     public void sendTextBtn(){
         System.out.println("发送文字");
+        sendArea.setText("");//清空发送台
+        acceptArea.appendText("");//接受台打印
+
 
     }
 
